e0db641dcec571031245b4bfa7205d45e614032b
apunktbau <a.bau@gmx.net>
2014-05-03 13:19:05 +0200

    initialize variants using default constructor

--- test-variant.cpp
+++ test-variant.cpp
@@ -6,16 +6,21 @@ class Foo {
     Foo (int) {}  // non-trivial constructor
 };
 
+class Bar {};
+
 int main () {
-  Variant <Foo,int,std::string> variant;
+  Variant <Foo,Bar,int,std::string> variant;
   
   variant.setAt <0> (Foo (5));                            // set by index
-  variant.setAt <1> (42);                                 // set by index
-  variant.setAt <2> (std::string ("hallo welt"));         // set by index
+  variant.setAt <2> (42);                                 // set by index
+  variant.setAt <3> (std::string ("hallo welt"));         // set by index
 
   variant.set <Foo>         (Foo (5));                    // set by type
   variant.set <int>         (42);                         // set by type
   variant.set <std::string> (std::string ("hello world"));// set by type
+
+  variant.init <Bar>         ();                          // initialize by type (using default ctor)
+  variant.init <std::string> ();                          // initialize by type (using default ctor)
 
   variant.get <std::string> ();                           // gives std::string&
   //variant.get <Foo> ();                                 // runtime error
@@ -26,20 +31,21 @@ int main () {
 
   assert ( variant.caseOf <bool> (                        // implicit type matching
               [] (Foo&)         { return false; }  
+            , [] (Bar&)         { return false; }  
             , [] (int&)         { return false; }
             , [] (std::string&) { return true ; }
             )
          );
 
   //variant.set <bool> (false);          // compilation fails (bool is not in variant)
-  //variant.setAt <2>   (12);            // compilation fails (incompatible types)
-  //variant.setAt <5>   (12);            // compilation fails (index out of bounds)
+  //variant.setAt <3>  (12);             // compilation fails (incompatible types)
+  //variant.setAt <5>  (12);             // compilation fails (index out of bounds)
 
-  Variant <Foo,int,std::string> variant2 (variant);       // (deep) copy construction
+  Variant <Foo,Bar,int,std::string> variant2 (variant);   // (deep) copy construction
 
   assert (variant2.is <std::string> () == true);
 
-  Variant <Foo,int,std::string> variant3;                 
+  Variant <Foo,Bar,int,std::string> variant3;                 
 
   variant3 = variant2;                                    // (deep) assignment
   
